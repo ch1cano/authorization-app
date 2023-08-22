73de8b6e59c7787cb0d610772faf0b1c5b833572
ch1cano <104975708+ch1cano@users.noreply.github.com>
2023-08-22 15:42:39 +0300

    fix layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,12 @@ import Image from 'next/image'
 import styles from './page.module.css'
 import Header from '@/components/Header/Header'
 import Footer from '@/components/Footer/Footer'
-import Main from '@/components/Main/Main'
+import Main from '@/app/pages/main/page'
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Header/>
       <Main/>
-      <Footer/>
     </main>
   )
 }
