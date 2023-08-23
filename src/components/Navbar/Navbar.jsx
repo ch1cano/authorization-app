335f5fadf4940d2bfcfbd67b6ee038c83b421d40
ch1cano <104975708+ch1cano@users.noreply.github.com>
2023-08-23 15:10:21 +0300

    add logo and fix

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,18 +1,25 @@
 import React from "react";
 import s from "./Navbar.module.css"
 import Link from "next/link";
+import Image from "next/image";
+import logo from "@/assets/favicon.png"
 
 const Navbar = () => {
     return <div className={s.wrapper}>
-        <Link className={s.home} href={"/pages/main"}>
-            <p >Home</p>
-        </Link>
-        <Link className={s.home} href={"/pages/news"}>
-            <p >News</p>
-        </Link>
-        <Link className={s.home} href={"/pages/profile"}>
-            <p >Profile</p>
-        </Link>
+        {/* <div className={s.logo_box}>
+            <Image className={s.logo} src={logo} />
+        </div> */}
+        <div className={s.home_box}>
+            <Link className={s.home} href={"/pages/main"}>
+                <p >Home</p>
+            </Link>
+            <Link className={s.home} href={"/pages/news"}>
+                <p >News</p>
+            </Link>
+            <Link className={s.home} href={"/pages/profile"}>
+                <p >Profile</p>
+            </Link>
+        </div>
     </div>;
 };
 
